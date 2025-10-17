7c36ab7ed65cd52b9dfbe94246b6a9ce8b811bd1
Victoria Forte <victoria.pocladova@gmail.com>
2025-10-17 16:44:39 -0300

    hotfix(ui): Remove duplicate component declarations in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { Tabs, Text, Card, Loader, ThemeProvider, Flex, Authenticator, View } from '@aws-amplify/ui-react';
-import { Franchises, Stores, Products } from './pages'; // Suponiendo que estos componentes existen
+import { Tabs, Text, Card, ThemeProvider, Flex, Authenticator, View } from '@aws-amplify/ui-react';
+import { Franchises, Stores, Products } from './pages';
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
@@ -34,13 +34,5 @@ function App() {
     </ThemeProvider>
   );
 }
-
-// Note: The original file did not have a definition for Franchises, Stores, and Products.
-// To make the code functional, we add some simple placeholder components.
-// If these files exist in the /pages folder, this part is not necessary.
-const Placeholder = ({ name }) => <Card><Text>{name} content will be displayed here.</Text></Card>;
-const Franchises = () => <Placeholder name="Franchises" />;
-const Stores = () => <Placeholder name="Stores" />;
-const Products = () => <Placeholder name="Products" />;
 
 export default App;
